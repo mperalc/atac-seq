87e813c665534fb928237413464887ed15cc2ca0
mperalc <martac@well.ox.ac.uk>
2017-07-18 18:18:13 +0100

    initial commit

---
+++ ATAC_and_GWAS.R
@@ -0,0 +1,6 @@
+# Investigating correlations and enrichment of ATAC-seq peaks and GWAS hits
+
+# How many ATAC-seq peaks are present inside GWAS credible regions? 
+
+# T2D DIAGRAM - 150,000 indiv imputed 1000Genomes Scott et al 2017
+
