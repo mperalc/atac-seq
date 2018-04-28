d8ad1a75172d8ee74567b18e845c2c8844fba4c4
mperalc <martac@well.ox.ac.uk>
2018-04-28 22:28:22 +0100

    clean up functions

--- SNPs_overlap_peaks.R
+++ SNPs_overlap_peaks.R
@@ -119,319 +119,150 @@ overlapPeak = subsetByOverlaps(peaks.gr,credset.gr)
 # # Displays nearest features to TSS
 # 
 # annotatedPeak = as.data.frame(annotatedPeak)
-
-annotate_peaks_TxDb = function(peaks.gr){
-  if(!any( is(peaks.gr)=="GRanges")){
-    stop('peak object is not GRanges')
-  }
-  
-  require(TxDb.Hsapiens.UCSC.hg19.knownGene)
-  txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
-  txdb
-  
-  # we can extract transcriptome data out of this object
-  # TX <- transcripts(txdb)
-  # 
-  # TX
-  # length(TX)
-  
-  GN = genes(txdb)
-  GN
-  length(GN)
-  
-  # intergenic
-  GN <- reduce(GN, ignore.strand=T)
-  intergenic <- gaps(GN)  # intergenic is just the complement of the genic, 
-  intergenic <- intergenic[strand(intergenic) == "*"] #This is important!!! otherwise you'll get an additional 2 entries per chromosome (one for each of + and -)
-  
-  # get promoters (certain distance from TSS)
-  PR <- promoters(txdb, upstream=2000, downstream=500)
-  PR
-  length(PR)
-  # exons
-  EX <- exons(txdb)
-  EX
-  length(EX)
-  # substract promoter parts, because they are overlapping?
-  
-  # coding - subset of exon
-  CD = cds(txdb)
-  CD
-  length(CD)
-  # substract promoter parts, because they are overlapping
-  
-  
-  # intron
-  IN = intronicParts(txdb)
-  IN
-  length(IN)
-  
-  # TX = reduce(TX, ignore.strand=T) # ignoring strand info
-  IN = reduce(IN, ignore.strand=T)
-  CD = reduce(CD, ignore.strand=T)
-  PR = reduce(PR, ignore.strand=T)
-  EX = reduce(EX, ignore.strand=T)
-  GN = reduce(GN, ignore.strand=T)
-  
-  # removing overlapping regions
-  length(intergenic)
-  intergenic = setdiff(intergenic , subsetByOverlaps( IN,intergenic))
-  length(intergenic)
-  intergenic = setdiff(intergenic , subsetByOverlaps( EX,intergenic))
-  length(intergenic)
-  intergenic = setdiff(intergenic , subsetByOverlaps( PR,intergenic))
-  length(intergenic) # slightly more sequences than before. Length!=length of sequence
-  
-  length(IN)
-  IN = setdiff(IN , subsetByOverlaps( EX,IN))
-  length(IN)
-  IN = setdiff(IN , subsetByOverlaps( PR,IN))
-  length(IN)
-  
-  length(EX)
-  EX = setdiff(EX, subsetByOverlaps(PR,EX))
-  length(EX)
-  
-  # now that I have all the features I'm interested in, I can intersect and plot the membership of 
-  # peaks
-  # peaks overlapping GWAS
-  # GWAS
-  # overall genome - compare this to published info to see I'm doing it right
-  
-  ## all peaks data
-  
-  # this way I end up with the whole peak sequence of those that overlap the features
-  PRallPeaks =subsetByOverlaps(peaks.gr,PR) # promoter
-  EXallPeaks = subsetByOverlaps(peaks.gr,EX) # exon
-  INallPeaks = subsetByOverlaps(peaks.gr,IN) # intron
-  intergenicAllPeaks = subsetByOverlaps(peaks.gr,intergenic) # intergenic
-  
-  sum(length(PRallPeaks),length(EXallPeaks),length(INallPeaks),length(intergenicAllPeaks))-
-    
-    length(subsetByOverlaps(EXallPeaks,PRallPeaks))-
-    length(subsetByOverlaps(INallPeaks,PRallPeaks))-
-    length(subsetByOverlaps(intergenicAllPeaks,PRallPeaks)) -
-    length(subsetByOverlaps(intergenicAllPeaks,EXallPeaks)) -
-    length(subsetByOverlaps(intergenicAllPeaks,INallPeaks))-
-    length(subsetByOverlaps(EXallPeaks,INallPeaks))
-  
-  # removing duplicates: prioritize assignment to PR 1st, EX 2nd, IN 3rd
-  EXallPeaks = setdiff(EXallPeaks,subsetByOverlaps(EXallPeaks,PRallPeaks)) # substracting PR peaks from the EX peaks
-  INallPeaks = setdiff(INallPeaks,subsetByOverlaps(INallPeaks,PRallPeaks)) # substracting PR peaks from the IN peaks
-  INallPeaks = setdiff(INallPeaks,subsetByOverlaps(INallPeaks,EXallPeaks)) # substracting EX peaks from the IN peaks
-  intergenicAllPeaks = setdiff(intergenicAllPeaks,subsetByOverlaps(intergenicAllPeaks,PRallPeaks)) # substracting PR peaks from the intergenic peaks
-  intergenicAllPeaks = setdiff(intergenicAllPeaks,subsetByOverlaps(intergenicAllPeaks,EXallPeaks)) # substracting EX peaks from the intergenic peaks
-  intergenicAllPeaks = setdiff(intergenicAllPeaks,subsetByOverlaps(intergenicAllPeaks,INallPeaks)) # substracting IN peaks from the intergenic peaks
-  
-  # % over 100% of peaks
-  all = sum(length(PRallPeaks),length(EXallPeaks),length(INallPeaks),length(intergenicAllPeaks))
-  
-  mylist <- list() 
-  
-  mylist[["PR"]] = (length(PRallPeaks)/all)*100
-  mylist[["EX"]] = (length(EXallPeaks)/all)*100
-  mylist[["IN"]] = (length(INallPeaks)/all)*100
-  mylist[["intergenic"]] = (length(intergenicAllPeaks)/all)*100 
-
-  df <- do.call("rbind",mylist) #combine all vectors into a matrix
-  df = as.data.frame(df)
-  colnames(df) = "value"
-  return(df)
-  
-  
-  combined = c(PRallPeaks,EXallPeaks,INallPeaks,intergenicAllPeaks)
-  setdiff(peaks.gr,combined) # it seems like unassigned peaks are intergenic
-  
-  # safer to remove them
-  peaks.gr = setdiff(peaks.gr,setdiff(peaks.gr,combined))
-  length(peaks.gr) == length(combined)
-  
-}
-
-# general genome
-annotate_whole_genome = function(){
-  require(TxDb.Hsapiens.UCSC.hg19.knownGene)
-  txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
-  
-  # GN = genes(txdb)
-  # GN
-  # length(GN)
-  # 
-  # # intergenic
-  # #GN <- reduce(GN, ignore.strand=T)
-  # intergenic <- gaps(GN)  # intergenic is just the complement of the genic, 
-  # intergenic <- intergenic[strand(intergenic) == "+" | strand(intergenic) == "-" ] #
-  # 
-  # # get promoters (certain distance from TSS)
-  # PR <- promoters(txdb, upstream=2000, downstream=500)
-  # PR
-  # length(PR)
-  # # exons
-  # EX <- exons(txdb)
-  # EX
-  # length(EX)
-  # # substract promoter parts, because they are overlapping?
-  # 
-  # # coding - subset of exon
-  # CD = cds(txdb)
-  # CD
-  # length(CD)
-  # # substract promoter parts, because they are overlapping
-  # 
-  # # intron
-  # IN = intronicParts(txdb)
-  # IN
-  # length(IN)
-  # 
-  # length(intergenic)
-  # intergenic = setdiff(intergenic , subsetByOverlaps( IN,intergenic))
-  # length(intergenic)
-  # intergenic = setdiff(intergenic , subsetByOverlaps( EX,intergenic))
-  # length(intergenic)
-  # intergenic = setdiff(intergenic , subsetByOverlaps( PR,intergenic))
-  # length(intergenic) # slightly more sequences than before. Length!=length of sequence
-  # 
-  # length(IN)
-  # IN = setdiff(IN , subsetByOverlaps( EX,IN))
-  # length(IN)
-  # IN = setdiff(IN , subsetByOverlaps( PR,IN))
-  # length(IN)
-  # 
-  # length(EX)
-  # EX = setdiff(EX, subsetByOverlaps(PR,EX))
-  # length(EX)
-  # 
-  # ######################### whole genome or just autosomes?
-  # EX_sum = sum(as.numeric(width(EX)))
-  # PR_sum = sum(as.numeric(width(PR)))
-  # IN_sum = sum(as.numeric(width(IN)))
-  # intergenic_sum = sum(as.numeric(width(intergenic)))
-  # total_sum = EX_sum + PR_sum + IN_sum + intergenic_sum  # this includes *both* strands, so half of that would be the length of genome
-  # (total_sum / 2)/1000000
-  # # my estimate is a bit under the latest reports (3,234.83 Mb)
-  # 
-  # # In this case the % of each annotation has been calculated by total bp and not by feature count (ATAC, SNP... binary measure not taking into account their size)
-  # # because obviously the intergenic regions for example are fewer than exons, but span more basepairs and therefore can potentially overlap more features
-  # # I believe that coding the ATAC by overlap w/o taking into account their length and assigning only to one feature 
-  # # (potentially the one where it would have more effect) facilitates the interpretation of the data and reflects the biology better
-  # # eg peaks overlapping promoters and introns are more likely to act on promoters, as open access for TFs
-  # 
-  # mylist <- list() 
-  # 
-  # mylist[["PR"]] = (PR_sum/total_sum)*100
-  # mylist[["EX"]] = (EX_sum/total_sum)*100
-  # mylist[["IN"]] = (IN_sum/total_sum)*100
-  # mylist[["intergenic"]] = (intergenic_sum/total_sum)*100
-  # 
-  # df <- do.call("rbind",mylist) #combine all vectors into a matrix
-  # df = as.data.frame(df)
-  # colnames(df) = "value"
-  
-  # results here give very low intron and very high intergenic
-  # unstranded calculations more similar to published data
-  # could the unstranded method be overestimating the amount of introns by counting complementary strands where the technical definition should be intergenic?
-  # Maybe the unstranded definition of annotations is more biologically relevant?
-  
- # return(df)
-  
-  ############3############ unstranded  #################################
-  GN = genes(txdb)
-  GN
-  length(GN)
-  
-  # intergenic
-  GN <- reduce(GN, ignore.strand=T)
-  intergenic <- gaps(GN)  # intergenic is just the complement of the genic, 
-  intergenic <- intergenic[strand(intergenic) == "*"] #This is important!!! otherwise you'll get an additional 2 entries per chromosome (one for each of + and -)
-  
-  # get promoters (certain distance from TSS)
-  PR <- promoters(txdb, upstream=2000, downstream=500)
-  PR
-  length(PR)
-  # exons
-  EX <- exons(txdb)
-  EX
-  length(EX)
-  # substract promoter parts, because they are overlapping?
-  
-  # coding - subset of exon
-  CD = cds(txdb)
-  CD
-  length(CD)
-  # substract promoter parts, because they are overlapping
-  
-  
-  # intron
-  IN = intronicParts(txdb)
-  IN
-  length(IN)
-  
-  # TX = reduce(TX, ignore.strand=T) # ignoring strand info
-  IN = reduce(IN, ignore.strand=T)
-  CD = reduce(CD, ignore.strand=T)
-  PR = reduce(PR, ignore.strand=T)
-  EX = reduce(EX, ignore.strand=T)
-  GN = reduce(GN, ignore.strand=T)
-  
-  # removing overlapping regions
-  length(intergenic)
-  intergenic = setdiff(intergenic , subsetByOverlaps( IN,intergenic))
-  length(intergenic)
-  intergenic = setdiff(intergenic , subsetByOverlaps( EX,intergenic))
-  length(intergenic)
-  intergenic = setdiff(intergenic , subsetByOverlaps( PR,intergenic))
-  length(intergenic) # slightly more sequences than before. Length!=length of sequence
-  
-  length(IN)
-  IN = setdiff(IN , subsetByOverlaps( EX,IN))
-  length(IN)
-  IN = setdiff(IN , subsetByOverlaps( PR,IN))
-  length(IN)
-  
-  length(EX)
-  EX = setdiff(EX, subsetByOverlaps(PR,EX))
-  length(EX)
-  
-  EX_sum = sum(as.numeric(width(EX)))
-  PR_sum = sum(as.numeric(width(PR)))
-  IN_sum = sum(as.numeric(width(IN)))
-  intergenic_sum = sum(as.numeric(width(intergenic)))
-  total_sum = EX_sum + PR_sum + IN_sum + intergenic_sum 
-  # my estimate is a bit under the latest reports (3,234.83 Mb)
-  
-  # In this case the % of each annotation has been calculated by total bp and not by feature count (ATAC, SNP... binary measure not taking into account their size)
-  # because obviously the intergenic regions for example are fewer than exons, but span more basepairs and therefore can potentially overlap more features
-  # I believe that coding the ATAC by overlap w/o taking into account their length and assigning only to one feature 
-  # (potentially the one where it would have more effect) facilitates the interpretation of the data and reflects the biology better
-  # eg peaks overlapping promoters and introns are more likely to act on promoters, as open access for TFs
-  
-  # for this reason promoters could be overestimated here (vs estimate of 0.5% by Alexander et al., Nat Rev Genet, 2010)
-  
-  mylist <- list() 
-  
-  mylist[["PR"]] = (PR_sum/total_sum)*100
-  mylist[["EX"]] = (EX_sum/total_sum)*100
-  mylist[["IN"]] = (IN_sum/total_sum)*100
-  mylist[["intergenic"]] = (intergenic_sum/total_sum)*100
-  
-  df <- do.call("rbind",mylist) #combine all vectors into a matrix
-  df = as.data.frame(df)
-  colnames(df) = "value"
-  return(df)
-}
+source('~/WTCHG/R scripts/atac-seq/annotate_GRanges_TxDb.R') # loading whoile genome annotation function
+# 
+# annotate_peaks_TxDb = function(peaks.gr){
+#   if(!any( is(peaks.gr)=="GRanges")){
+#     stop('peak object is not GRanges')
+#   }
+#   
+#   require(TxDb.Hsapiens.UCSC.hg19.knownGene)
+#   txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
+#   txdb
+#   
+#   # we can extract transcriptome data out of this object
+#   # TX <- transcripts(txdb)
+#   # 
+#   # TX
+#   # length(TX)
+#   
+#   GN = genes(txdb)
+#   GN
+#   length(GN)
+#   
+#   # intergenic
+#   GN <- reduce(GN, ignore.strand=T)
+#   intergenic <- gaps(GN)  # intergenic is just the complement of the genic, 
+#   intergenic <- intergenic[strand(intergenic) == "*"] #This is important!!! otherwise you'll get an additional 2 entries per chromosome (one for each of + and -)
+#   
+#   # get promoters (certain distance from TSS)
+#   PR <- promoters(txdb, upstream=2000, downstream=500)
+#   PR
+#   length(PR)
+#   # exons
+#   EX <- exons(txdb)
+#   EX
+#   length(EX)
+#   # substract promoter parts, because they are overlapping?
+#   
+#   # coding - subset of exon
+#   CD = cds(txdb)
+#   CD
+#   length(CD)
+#   # substract promoter parts, because they are overlapping
+#   
+#   
+#   # intron
+#   IN = intronicParts(txdb)
+#   IN
+#   length(IN)
+#   
+#   # TX = reduce(TX, ignore.strand=T) # ignoring strand info
+#   IN = reduce(IN, ignore.strand=T)
+#   CD = reduce(CD, ignore.strand=T)
+#   PR = reduce(PR, ignore.strand=T)
+#   EX = reduce(EX, ignore.strand=T)
+#   GN = reduce(GN, ignore.strand=T)
+#   
+#   # removing overlapping regions
+#   length(intergenic)
+#   intergenic = setdiff(intergenic , subsetByOverlaps( IN,intergenic))
+#   length(intergenic)
+#   intergenic = setdiff(intergenic , subsetByOverlaps( EX,intergenic))
+#   length(intergenic)
+#   intergenic = setdiff(intergenic , subsetByOverlaps( PR,intergenic))
+#   length(intergenic) # slightly more sequences than before. Length!=length of sequence
+#   
+#   length(IN)
+#   IN = setdiff(IN , subsetByOverlaps( EX,IN))
+#   length(IN)
+#   IN = setdiff(IN , subsetByOverlaps( PR,IN))
+#   length(IN)
+#   
+#   length(EX)
+#   EX = setdiff(EX, subsetByOverlaps(PR,EX))
+#   length(EX)
+#   
+#   # now that I have all the features I'm interested in, I can intersect and plot the membership of 
+#   # peaks
+#   # peaks overlapping GWAS
+#   # GWAS
+#   # overall genome - compare this to published info to see I'm doing it right
+#   
+#   ## all peaks data
+#   
+#   # this way I end up with the whole peak sequence of those that overlap the features
+#   PRallPeaks =subsetByOverlaps(peaks.gr,PR) # promoter
+#   EXallPeaks = subsetByOverlaps(peaks.gr,EX) # exon
+#   INallPeaks = subsetByOverlaps(peaks.gr,IN) # intron
+#   intergenicAllPeaks = subsetByOverlaps(peaks.gr,intergenic) # intergenic
+#   
+#   sum(length(PRallPeaks),length(EXallPeaks),length(INallPeaks),length(intergenicAllPeaks))-
+#     
+#     length(subsetByOverlaps(EXallPeaks,PRallPeaks))-
+#     length(subsetByOverlaps(INallPeaks,PRallPeaks))-
+#     length(subsetByOverlaps(intergenicAllPeaks,PRallPeaks)) -
+#     length(subsetByOverlaps(intergenicAllPeaks,EXallPeaks)) -
+#     length(subsetByOverlaps(intergenicAllPeaks,INallPeaks))-
+#     length(subsetByOverlaps(EXallPeaks,INallPeaks))
+#   
+#   # removing duplicates: prioritize assignment to PR 1st, EX 2nd, IN 3rd
+#   EXallPeaks = setdiff(EXallPeaks,subsetByOverlaps(EXallPeaks,PRallPeaks)) # substracting PR peaks from the EX peaks
+#   INallPeaks = setdiff(INallPeaks,subsetByOverlaps(INallPeaks,PRallPeaks)) # substracting PR peaks from the IN peaks
+#   INallPeaks = setdiff(INallPeaks,subsetByOverlaps(INallPeaks,EXallPeaks)) # substracting EX peaks from the IN peaks
+#   intergenicAllPeaks = setdiff(intergenicAllPeaks,subsetByOverlaps(intergenicAllPeaks,PRallPeaks)) # substracting PR peaks from the intergenic peaks
+#   intergenicAllPeaks = setdiff(intergenicAllPeaks,subsetByOverlaps(intergenicAllPeaks,EXallPeaks)) # substracting EX peaks from the intergenic peaks
+#   intergenicAllPeaks = setdiff(intergenicAllPeaks,subsetByOverlaps(intergenicAllPeaks,INallPeaks)) # substracting IN peaks from the intergenic peaks
+#   
+#   # % over 100% of peaks
+#   all = sum(length(PRallPeaks),length(EXallPeaks),length(INallPeaks),length(intergenicAllPeaks))
+#   
+#   mylist <- list() 
+#   
+#   mylist[["PR"]] = (length(PRallPeaks)/all)*100
+#   mylist[["EX"]] = (length(EXallPeaks)/all)*100
+#   mylist[["IN"]] = (length(INallPeaks)/all)*100
+#   mylist[["intergenic"]] = (length(intergenicAllPeaks)/all)*100 
+# 
+#   df <- do.call("rbind",mylist) #combine all vectors into a matrix
+#   df = as.data.frame(df)
+#   colnames(df) = "value"
+#   return(df)
+#   
+#   
+#   combined = c(PRallPeaks,EXallPeaks,INallPeaks,intergenicAllPeaks)
+#   setdiff(peaks.gr,combined) # it seems like unassigned peaks are intergenic
+#   
+#   # safer to remove them
+#   peaks.gr = setdiff(peaks.gr,setdiff(peaks.gr,combined))
+#   length(peaks.gr) == length(combined)
+#   
+# }
 
 # ATAC peaks and GWAS credible set SNPs
-allPeaksAnnotated = annotate_peaks_TxDb(peaks.gr) 
+allPeaksAnnotated = annotate_GRanges_TxDb(peaks.gr) 
 allPeaksAnnotated$anno = rownames(allPeaksAnnotated) # shaping for ggplot
 allPeaksAnnotated$type= "All ATAC-seq peaks"
-peaksOverGWASAnnotated = annotate_peaks_TxDb(overlapPeak) 
+peaksOverGWASAnnotated = annotate_GRanges_TxDb(overlapPeak) 
 peaksOverGWASAnnotated$anno = rownames(peaksOverGWASAnnotated)
 peaksOverGWASAnnotated$type= "ATAC-seq peaks overlapping credible sets"
-GWASAnnotated = annotate_peaks_TxDb(credset.gr)
+GWASAnnotated = annotate_GRanges_TxDb(credset.gr)
 GWASAnnotated$anno = rownames(GWASAnnotated)
 GWASAnnotated$type= "All SNPs in credible sets"
 
-whole = annotate_whole_genome() # whole genome annotation
+source('~/WTCHG/R scripts/annotate_whole_genome_TxDb.R') # loading whole genome annotation function
+
+whole = annotate_whole_genome_TxDb() # whole genome annotation
 whole$anno = rownames(whole)
 whole$type = "Whole genome"
   
@@ -447,7 +278,7 @@ pal=c("#96165a","#bf3b5c","#3D8DC1","#95C8EB")   # red and blue scale
 
 source('~/WTCHG/R scripts/plotting/ggplot_stacked_barplot_percent.R') # loading barplot function
 
-p = ggplot_stacked_barplot_percent(combined,pal )
+p = ggplot_stacked_barplot_percent(combined,pal,0.75 )
 
 png("/Users/Marta/Documents/WTCHG/DPhil/Plots/atac-seq/TxDb_annotation_peaks_credible_set_T2D_DIAGRAM.png", type="cairo",
     width=14,height=8,units="in",res=500,pointsize = 12)
